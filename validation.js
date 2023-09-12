4fb1f238be8ab367bec92a5845b8f4ce7a1cbfdf
ShivaniJadhav <97583560+ShivuJ@users.noreply.github.com>
2023-09-12 23:16:30 +0530

    Replaced JavaScript with JQuery

--- validation.js
+++ validation.js
@@ -1,62 +1,70 @@
-function validationForm() {
-    var name = document.getElementById("name").value;
-    var email = document.getElementById("email").value;
-    var contact = document.getElementById("contact").value;
-    var firstPass = document.getElementById("password").value;
-    var secondPass = document.getElementById("password2").value;
-
-    if (name.trim() === "") {
-        // alert("Please enter valid name");
-        document.getElementById("uname").style.visibility = "visible";
-        document.getElementById("emailId").style.visibility = "hidden";
-        document.getElementById("contactNo").style.visibility = "hidden";
-        document.getElementById("pass").style.visibility = "hidden";
-        document.getElementById("pass2").style.visibility = "hidden";
-        return false;
-    }
-    else if (email.trim() === "") {
-        //alert("Please enter valid email");
-        document.getElementById("emailId").style.visibility = "visible";
-        document.getElementById("uname").style.visibility = "hidden";
-        document.getElementById("contactNo").style.visibility = "hidden";
-        document.getElementById("pass").style.visibility = "hidden";
-        document.getElementById("pass2").style.visibility = "hidden";
-        return false;
-    }
-    else if (contact.length > 10 || contact.length < 10 || contact.trim() === "") {
-        //alert("Please enter valid contact number");
-        document.getElementById("contactNo").style.visibility = "visible";
-        document.getElementById("uname").style.visibility = "hidden";
-        document.getElementById("emailId").style.visibility = "hidden";
-        document.getElementById("pass").style.visibility = "hidden";
-        document.getElementById("pass2").style.visibility = "hidden";
-        return false;
-    }
-    else if (firstPass.trim() === "" || firstPass.length < 7) {
-        //alert("Please enter valid password");
-        document.getElementById("pass").style.visibility = "visible";
-        document.getElementById("uname").style.visibility = "hidden";
-        document.getElementById("emailId").style.visibility = "hidden";
-        document.getElementById("contactNo").style.visibility = "hidden";
-        document.getElementById("pass2").style.visibility = "hidden";
-        return false;
-    }
-    else if (firstPass != secondPass || secondPass.trim() === "") {
-        //alert("Password does not match")
-        document.getElementById("pass2").style.visibility = "visible";
-        document.getElementById("uname").style.visibility = "hidden";
-        document.getElementById("emailId").style.visibility = "hidden";
-        document.getElementById("contactNo").style.visibility = "hidden";
-        document.getElementById("pass").style.visibility = "hidden";
-        return false;
-    }
-    else {
-        alert("Sucessfully Registered...");
-        document.getElementById("uname").style.visibility = "hidden";
-        document.getElementById("emailId").style.visibility = "hidden";
-        document.getElementById("contactNo").style.visibility = "hidden";
-        document.getElementById("pass").style.visibility = "hidden";
-        document.getElementById("pass2").style.visibility = "hidden";
-        return true;
-    }
-}
+// $(document).ready(function(){
+    $("#submit").click(function() {
+        var name = $("#name").val();
+        var email = $("#email").val();
+        var contact = $("#contact").val();
+        var firstpass = $("#password").val();
+        var secondpass = $("#password2").val();
+    
+        if (name.trim() === "") {
+            // alert("Please enter valid name");
+            $("#uname").css('visibility', 'visible');
+            $("#emailId").css('visibility', 'hidden');
+            $("#contactNo").css('visibility', 'hidden');
+            $("#pass").css('visibility', 'hidden');
+            $("#pass2").css('visibility', 'hidden');
+            return false;
+        }
+        else if (email.trim() === "") {
+            //alert("Please enter valid email");
+            $("#emailId").css('visibility', 'visible');
+            $("#uname").css('visibility', 'hidden');
+            $("#contactNo").css('visibility', 'hidden');
+            $("#pass").css('visibility', 'hidden');
+            $("#pass2").css('visibility', 'hidden');
+            return false;
+        }
+        else if (contact.length > 10 || contact.length < 10 || contact.trim() === "") {
+            // alert("Please enter valid contact number");
+            $("#contactNo").css('visibility', 'visible');
+            $("#uname").css('visibility', 'hidden');
+            $("#emailId").css('visibility', 'hidden');
+            $("#pass").css('visibility', 'hidden');
+            $("#pass2").css('visibility', 'hidden');
+            $("#contact").keyup(function(){
+                if(contact.length > 10 || contact.length < 10){
+                    $("#contactNo").css('visibility', 'hidden');
+                    $("#contactValidation").css('visibility', 'visible');
+                };
+            });
+            return false;
+        }
+        else if (firstpass.trim() === "" || firstpass.length < 7) {
+            //alert("Please enter valid #password");
+            $("#pass").css('visibility', 'visible');
+            $("#uname").css('visibility', 'hidden');
+            $("#emailId").css('visibility', 'hidden');
+            $("#contactNo").css('visibility', 'hidden');
+            $("#pass2").css('visibility', 'hidden');
+            return false;
+        }
+        else if (firstpass != secondpass || secondpass.trim() === "") {
+            //alert("#password does not match")
+            $("#pass2").css('visibility', 'visible');
+            $("#uname").css('visibility', 'hidden');
+            $("#emailId").css('visibility', 'hidden');
+            $("#contactNo").css('visibility', 'hidden');
+            $("#pass").css('visibility', 'hidden');
+            return false;
+        }
+        else {
+            alert("Sucessfully Registered...");
+            $("#uname").css('visibility', 'hidden');
+            $("#emailId").css('visibility', 'hidden');
+            $("#contactNo").css('visibility', 'hidden');
+            $("#pass").css('visibility', 'hidden');
+            $("#pass2").css('visibility', 'hidden');
+            return true;
+        }
+    });
+// });
